b03d0e41671076d50d19a725baca9e436f0aa06a
Michele Baldessari <michele@acksyn.org>
2023-05-07 14:36:44 +0200

    more changes

--- bandini.sh
+++ bandini.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -ex
 #
-INDEX_IMAGES="$*"
 : ${IIB_SOURCE=registry-proxy.engineering.redhat.com}
 : ${MIRROR_NAMESPACE=openshift-marketplace}
 : ${IIB_NAMESPACE=abeekhof}
 : ${FILTER=gitops}
 : ${INSTALL=0}
 : ${MIRROR_ARGS=""}
+
+if [ $# -ne 1 ]; then
+        echo "Call $0 with one parameter: the IIB number"
+        exit 1
+fi
 
 function pre_check() {
         if ! oc whoami -t; then
@@ -24,7 +28,7 @@ function configure_internal_registry() {
         password=$1
         echo "Enabling the built-in registry"
         oc patch configs.imageregistry.operator.openshift.io/cluster --patch '{"spec":{"defaultRoute":true}}' --type=merge
-        export MIRROR_TARGET=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}') 
+        export MIRROR_TARGET=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
         echo "Giving permission for the cluster to access the registries"
         oc patch image.config.openshift.io/cluster --patch "{\"spec\":{\"registrySources\":{\"allowedRegistries\":[ \"quay.io\", \"registry.redhat.io\", \"registry-proxy.engineering.redhat.com\", \"image-registry.openshift-image-registry.svc:5000\", \"$MIRROR_TARGET\"]}}}" --type=merge
         oc patch image.config.openshift.io/cluster --patch "{\"spec\":{\"registrySources\":{\"insecureRegistries\":[ \"registry-proxy.engineering.redhat.com\", \"image-registry.openshift-image-registry.svc:5000\", \"$MIRROR_TARGET\"]}}}" --type=merge
@@ -60,36 +64,38 @@ function apply_image_digest_mirror_set() {
         sed -e "s@TEMPLATEROUTE@${IIB_TARGET}@" -e "s@TEMPLATESOURCE@${IIB_SOURCE}/rh-osbs@" "${MIRRORSET}" | oc apply -f-
 }
 
-
-pass=$(oc whoami -t)
-PULLSECRET=$PWD/.dockerconfigjson
-
-pre_check
-configure_internal_registry "${pass}"
-
-
-for IIB_ENTRY in $(echo $INDEX_IMAGES | tr ',' '\n'); do 
-        echo "Processing $IIB_ENTRY" 
-        export IIB=$(echo $IIB_ENTRY | sed 's/.*://') 
-        export IIB_PATH=$PWD/manifests-iib-$IIB 
+function install_new_iib() {
+        echo "Processing $INDEX_IMAGE"
+        export IIB=$(echo $INDEX_IMAGE | sed 's/.*://')
+        export IIB_PATH=$PWD/manifests-iib-$IIB
         rm -rf $IIB_PATH && mkdir $IIB_PATH
         pushd $IIB_PATH
 
         echo "Creating $IIB manifests"
         oc adm catalog mirror --insecure --manifests-only --to-manifests=. $IIB_SOURCE/rh-osbs/iib:$IIB $IIB_SOURCE/rh-osbs 2>&1 | tee catalog.log
 
-        echo "Mirroring $IIB catalog" 
+        echo "Mirroring $IIB catalog"
         # FIXME(bandini): this sometimes fails and needs a retry mechanism
-        oc image mirror -a $PULLSECRET $IIB_SOURCE/rh-osbs/iib:$IIB=${IIB_TARGET}iib:$IIB --insecure --keep-manifest-list 2>&1 | tee iib.log 
+        oc image mirror -a $PULLSECRET $IIB_SOURCE/rh-osbs/iib:$IIB=${IIB_TARGET}iib:$IIB --insecure --keep-manifest-list 2>&1 | tee iib.log
 
-        sed -i "s/name: iib$/name: iib-$IIB/" catalogSource.yaml  
+        sed -i "s/name: iib$/name: iib-$IIB/" catalogSource.yaml
         sed -i "s@$IIB_SOURCE/rh-osbs/rh-osbs-@$IIB_TARGET@" catalogSource.yaml
         sed -i "s/grpc/grpc\n  displayName: IIB $IIB/"  catalogSource.yaml
-        oc apply -f $IIB_PATH/catalogSource.yaml  
+        oc apply -f $IIB_PATH/catalogSource.yaml
         wait_for_new_catalog openshift-gitops-operator "${IIB}"
-	      channel=$(oc get -n "${MIRROR_NAMESPACE}" packagemanifests -l "catalog=iib-$IIB" --field-selector 'metadata.name=openshift-gitops-operator' \
-                -o jsonpath='{.items[0].status.defaultChannel }')
         popd
-done 
+}
 
+pre_check
+
+pass=$(oc whoami -t)
+PULLSECRET=$PWD/.dockerconfigjson
+INDEX_IMAGE="$1"
+
+configure_internal_registry "${pass}"
+install_new_iib
+channel=$(oc get -n "${MIRROR_NAMESPACE}" packagemanifests -l "catalog=iib-$IIB" --field-selector 'metadata.name=openshift-gitops-operator' \
+        -o jsonpath='{.items[0].status.defaultChannel }')
+
+apply_image_digest_mirror_set
 
